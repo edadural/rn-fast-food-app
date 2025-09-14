9402b2e07b5627dc3047f01f4bb02768f64fe27b
Eda Dural <edaadurall@gmail.com>
2025-09-14 10:02:36 +0300

    setup fonts-assets

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
+      <Text className="text-5xl text-center text-primary font-quicksand-bold">
         Welcome to my React Native App!
       </Text>
     </View>
