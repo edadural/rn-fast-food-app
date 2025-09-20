9b0fcf566c3265c7e7aeea1a6746e19a563d0be7
Eda Dural <edaadurall@gmail.com>
2025-09-20 08:47:57 +0300

    search screen

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { CreateUserPrams, SignInParams } from "@/type";
+import { CreateUserPrams, GetMenuParams, SignInParams } from "@/type";
 import {
   Account,
   Avatars,
@@ -88,6 +88,38 @@ export const getCurrentUser = async (): Promise<any> => {
     if (!currentUser) throw Error;
 
     return currentUser.documents[0];
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+  try {
+    const queries: string[] = [];
+
+    if (category) queries.push(Query.equal("category", category));
+    if (query) queries.push(Query.search("name", query));
+
+    const menus = await databases.listDocuments(
+      appwriteConfig.databaseId!,
+      appwriteConfig.menuCollectionId!,
+      queries
+    );
+
+    return menus.documents;
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    const categories = await databases.listDocuments(
+      appwriteConfig.databaseId!,
+      appwriteConfig.categoriesCollectionId!
+    );
+
+    return categories.documents;
   } catch (error) {
     throw new Error(error as string);
   }
